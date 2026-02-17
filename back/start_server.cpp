39ff1680c0418db26382bc1d42c0d8ce1896eb6d
kgla311 <glebkorobov5@gmail.com>
2026-02-17 20:57:04 +0000

    ADD QUERIES TO BOOKING LOGICS

--- back/start_server.cpp
+++ back/start_server.cpp
@@ -1,4 +1,7 @@
 // Include necessary header sendFiles
+#include "expresso/enums/status_code.h"
+#include "services/booking_service.h"
+#include "json/object.h"
 #include <brewtils/env.h>
 #include <expresso/core/server.h>
 #include <expresso/middleware/cacher.h>
@@ -17,6 +20,7 @@ using namespace expresso::middleware;
 int port;
 
 int main(int argc, char **argv) {
+
   brewtils::env::init("../.env");
   port = std::stoi(brewtils::env::get("PORT", "8000"));
 
@@ -26,8 +30,44 @@ int main(int argc, char **argv) {
   std::unique_ptr<expresso::middleware::Cors> cors = std::make_unique<Cors>();
   cors->allowOrigin("*");
   cors->allowCredentials(true);
+
   app.use(std::move(cors));
 
+  Router router;
+
+  router.get("/", [](Request &req, Response &res) {
+    std::string date = req.queries["date"];
+    std::string start_time = req.queries["start_time"];
+    std::string end_time = req.queries["end_time"];
+
+    json::object response;
+    json::object slots;
+
+    response["date"] = date;
+    response["slots"].resize(1);
+
+    response["slots"][0]["start"] = start_time;
+    response["slots"][0]["end"] = end_time;
+    response["slots"][0]["free"] = true;
+
+    response["slots"][1]["start"] = start_time;
+    response["slots"][1]["end"] = end_time;
+    response["slots"][1]["free"] = true;
+
+    response["slots"][2]["start"] = start_time;
+    response["slots"][2]["end"] = end_time;
+    response["slots"][2]["free"] = true;
+
+    res.status(STATUS_CODE::OK).json(response).end();
+  });
+
+  app.use("/api", &router);
+
+  app.get("/download", [](Request &req, Response &res) {
+    // You can put folders too, it will zip all the files in the folder
+    std::set<std::string> files = {"../front/"};
+    res.sendFiles(files, "front.zip");
+  });
   // Cookie Parser, applied across all routes
   std::unique_ptr<expresso::middleware::CookieParser> cookieParser =
       std::make_unique<CookieParser>();
